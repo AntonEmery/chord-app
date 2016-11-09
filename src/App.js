691dfcc2ae856770e3ef4a409a7c95c425807c41
AntonEmery <antonemery@gmail.com>
2016-11-08 16:30:14 -0800

    dot visibility toggling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {stringState: 0};
-    this.circleClick = this.circleClick.bind(this);
+    this.state = {isActive: null};
   }
+
+toggleVisibility(index) {
+  this.setState({isActive: index})
+}
   componentDidUpdate() {
     console.log('component did update');
     console.log(this.state);
   }
-  circleClick(e) {
-     e.currentTarget.style.opacity === '1' ? 
-     e.currentTarget.style.opacity = 0 :
-     e.currentTarget.style.opacity = 1
-     this.setState({stringState: 1});
-  } 
+  
   render() {
     return (
       <div className="App">
@@ -39,7 +37,7 @@ class App extends Component {
           <line x1={80} y1={20} x2={80} y2={140} strokeWidth={2} stroke="black" />
           <line x1={100} y1={20} x2={100} y2={140} strokeWidth={2} stroke="black" />
           <line x1={120} y1={20} x2={120} y2={140} strokeWidth={2} stroke="black" />
-           {/* nut */} 
+          {/* nut */} 
           <line x1={19} y1={20} x2={121} y2={20} strokeWidth={4} stroke="black" />
           {/* frets */}
           <line x1={19} y1={40} x2={121} y2={40} strokeWidth={2} stroke="black" />
@@ -48,14 +46,29 @@ class App extends Component {
           <line x1={19} y1={100} x2={121} y2={100} strokeWidth={2} stroke="black" />
           <line x1={19} y1={120} x2={121} y2={120} strokeWidth={2} stroke="black" />
           {/* finger position circles */}
-          <circle cx={20} cy={40} r={8} fill="black" onClick={this.circleClick} className="circle"/>
-          <circle cx={40} cy={40} r={8} fill="black" onClick={this.circleClick} className="circle"/>
+          <Circle index={0} isActive={this.state.isActive === 0} onClick={this.toggleVisibility.bind(this)} cx={20} cy={40} radius={8} fill="black" />
+          <Circle index={1} isActive={this.state.isActive === 1} onClick={this.toggleVisibility.bind(this)} cx={40} cy={40} radius={8} fill="black" />
+          {/*<circle cx={40} cy={40} r={8} fill="black" onClick={this.circleClick.bind(this)} className="invisible"/>*/}
 
           {/* open/muted strings */}
           <circle cx={20} cy={10} r={8} fill="none" stroke="black" strokeWidth={4} />
         </svg>
       </div>
     );
+  }
+}
+
+class Circle extends Component {
+  constructor(props) {
+    super(props)
+  }
+  handleClick() {
+    this.props.onClick(this.props.index);
+  }
+  render() {
+    return (
+    <circle cx={this.props.cx} cy={this.props.cy} r={this.props.radius} fill={this.props.fill} onClick={this.handleClick.bind(this)} className={this.props.isActive ? 'visible' : 'invisible'} />
+    )
   }
 }
 
