aa98e639939569204da171e3232ce3787129512d
AntonEmery <antonemery@gmail.com>
2016-12-25 09:34:31 -0800

    serving static html file

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from 'express';
+import path from 'path';
 let app = express();
 
-app.get('/', (req, res) => { res.send('Hello World') });
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')) );
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
-})
+app.listen(3000, () => console.log('listening on port 3000'));
