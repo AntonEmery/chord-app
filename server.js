f51803fc72bce490ded6ae09b0f850e0b532b3c2
AntonEmery <antonemery@gmail.com>
2016-12-26 08:13:02 -0800

    successfully connecting to local postgresql db

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import routes from './routes.js';
 let app = express();
+var Sequelize = require('sequelize');
+
+var sequelize =  new Sequelize('postgres://username:password@localhost:5432/chordapp');
 
 app.use(routes);
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
 
 app.listen(3000, () => console.log('listening on port 3000'));
