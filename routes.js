5982dbedbab27bb4871eac6f99dc28ab3600b0e7
AntonEmery <antonemery@gmail.com>
2017-01-01 22:55:39 -0800

    returning data from database

--- routes.js
+++ routes.js
@@ -2,15 +2,8 @@ import express from 'express';
 import path from 'path';
 let router = express.Router();
 
-
 router.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')) );
 router.get('/home', (req, res) => res.send('home route'));
 router.get('/about', (req, res) => res.send('about route'));
-
-
-
-
-
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import credentials from './credentials.js';
 import Sequelize from 'sequelize';
 let app = express();
 
-var sequelize =  new Sequelize(`postgres://${credentials.username}:${credentials.password}@localhost:5432/chordapp`);
+//instantiate new sequelize instance
+var db =  new Sequelize(`postgres://${credentials.username}:${credentials.password}@localhost:5432/chordapp`);
 
 app.use(routes);
 
-
-sequelize
+//test database connection
+db
   .authenticate()
   .then(function(err) {
     console.log('Connection has been established successfully.');
@@ -18,5 +19,44 @@ sequelize
   .catch(function (err) {
     console.log('Unable to connect to the database:', err);
   });
+
+
+//define users table
+var Users = db.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.TEXT
+  },
+  email: {
+    type: Sequelize.TEXT
+  }
+},
+{
+  timestamps: false
+});
+
+// Users.sync().then(function() {
+//   var data = {
+//     id: 2,
+//     name: 'Gilbert',
+//     email: 'gilbert@dog.com'
+//   }
+//
+//   Users.create(data).then(function(data) {
+//     console.log(data.get());
+//   })
+// });
+
+//returns are records for users table
+Users.findAll().then(function(users) {
+  users.forEach(function(val, index) {
+    console.log(users[index].dataValues);
+  })
+})
+
 
 app.listen(3000, () => console.log('listening on port 3000'));
