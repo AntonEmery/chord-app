1df63fbeec8dc050a7a47236c3154ab50ff6973e
AntonEmery <antonemery@gmail.com>
2017-06-13 21:16:29 -0700

    basic routing works

---
+++ src/components/ChordSheets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ChordSheets extends Component {
+  render() {
+    return (
+      <p>Chord Sheets</p>
+    )
+  }
+}
+
+export default ChordSheets
