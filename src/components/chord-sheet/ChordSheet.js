b8b64bfe2242471285908d8fb3898a2c219971d0
AntonEmery <antonemery@gmail.com>
2017-06-20 20:07:58 -0700

    added pure css

--- src/components/chord-sheet/ChordSheet.js
+++ src/components/chord-sheet/ChordSheet.js
@@ -3,9 +3,6 @@ import '../../App.css'
 import axios from 'axios'
 
 import Util from '../../Util.js'
-
-
-
 import AddChord from '../AddChord'
 import SaveButton from '../SaveButton'
 import ChordTemplate from '../ChordTemplate'
@@ -93,7 +90,7 @@ class ChordSheet extends Component {
 
   render() {
     return (
-      <div>
+      <div className="chord-sheet-wrapper">
       <div className="App">
         {this.state.chords.map((chord, index) => {
           return (<ChordTemplate key={index} id={index} state={chord} toggleVisibility={this.toggleVisibility} deleteChord={this.deleteChord}/>)
