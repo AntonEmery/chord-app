d8d2a2cf04ae255012ca9605c5927e96de290e06
AntonEmery <antonemery@gmail.com>
2018-07-11 08:37:23 -0700

    associated chord sheet with user, writing to db

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const ChordSheet = mongoose.model('ChordSheet');
 
-exports.createUser = (req, res) => {
-  const user = new User(req.body);
-  user.save();
+exports.createUser = async (req, res) => {
+  const chordSheet = new ChordSheet({title: 'Test Chord Sheet', chords: [['Em','0','2','2','0','0','0' ]]});
+  console.log('before' + chordSheet);
+  await chordSheet.save();
+  console.log('after' + chordSheet);
+  res.send('done');
+  const user = new User(Object.assign({}, req.body, { chordSheets: [chordSheet] }));
+  await user.save();
 }
