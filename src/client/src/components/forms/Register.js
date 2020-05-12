3f735b90df9242d5c1dc51cec1fafe52f99e61c3
AntonEmery <antonemery@gmail.com>
2020-05-11 19:14:05 -0700

    Don't submit registration form unless pwds match

--- src/client/src/components/forms/Register.js
+++ src/client/src/components/forms/Register.js
@@ -12,11 +12,18 @@ class Register extends Component {
       email: '',
       password: '',
       confirmedPassword: '',
+      passwordMismatch: false,
     }
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
+
+    if (this.state.password !== this.state.confirmedPassword) {
+      this.setState({ passwordMismatch: true });
+      return;
+    }
+
     axios.post(`${process.env.REACT_APP_API_URL}/register`, {
       name: this.state.name,
       email: this.state.email,
@@ -31,7 +38,6 @@ class Register extends Component {
     const formIncomplete = Object.values(this.state).some((key) => {
       return key.length === 0;
     })
-    console.log(formIncomplete)
     this.setState({ [event.target.name]: event.target.value })
   }
 
@@ -45,20 +51,21 @@ class Register extends Component {
         <form className="register">
           <div className="card__input-item">
             <label htmlFor="name">Name</label>
-            <input name="name" onChange={this.handleChange} type="text" placeholder="Username" />
+            <input name="name" onChange={this.handleChange} type="text" placeholder="Username" value={this.state.name}/>
           </div>
           <div className="card__input-item">
             <label htmlFor="email">Email</label>
-            <input name="email" onChange={this.handleChange} type="text" placeholder="Email" />
+            <input name="email" onChange={this.handleChange} type="text" placeholder="Email" value={this.state.text} />
           </div>
           <div className="card__input-item">
             <label htmlFor="password">Password</label>
-            <input name="password" onChange={this.handleChange} type="password" placeholder="Password" />
+            <input name="password" onChange={this.handleChange} type="password" placeholder="Password" value={this.state.password} />
           </div>
           <div className="card__input-item">
             <label htmlFor="confirmedPassword">Confirm Password</label>
-            <input name="confirmedPassword" onChange={this.handleChange} type="password" placeholder="Confirm Password" />
+            <input name="confirmedPassword" onChange={this.handleChange} type="password" placeholder="Confirm Password" value={this.state.confirmedPassword} />
           </div>
+          {this.state.passwordMismatch ? <p>Passwords must match</p> : ''}
           <button type="submit" onClick={this.handleSubmit} disabled={formIncomplete} className="button button--grey button--med">Create Account</button>
         </form>
       </div>
